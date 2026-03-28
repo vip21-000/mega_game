72f635ee8f1f33c47430436a7ca02bea0f7985f2
Сергій Гриндула <serg91007@gmal.com>
2026-03-28 17:49:30 +0200

    the settings button now works, and the menu is complete

--- scenes/menu/settings.gd
+++ scenes/menu/settings.gd
@@ -11,16 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-
-
-
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
-
-
-func _on_exit_pressed() -> void:
-	get_tree().quit()
-
-
-func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/menu/settings.tscn")
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu/menu.tscn")
